3a9eb067eb5cd04943eb98efbaae60238e2abf8c
Abhimanyu Shahi <abhimanyushahi88@gmail.com>
2023-05-03 13:45:07 +0530

    modified index file

--- index.js
+++ index.js
@@ -1,13 +1,30 @@
-var express = require('express')
-var app = express()
+/*
+ * Copyright 2020 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
-app.set('port', (process.env.PORT || 5000))
-app.use(express.static(__dirname + '/public'))
+ const express = require('express');
 
-app.get('/', function(request, response) {
-  response.send('Hello World!')
-})
+const app = express();
 
-app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
-})
+app.get('/', (req, res) => {
+        res.send('Hello World!');
+    });
+
+const port = 3000;
+const server = app.listen(port, () => {
+        console.log('listening on port %s.\n', server.address().port);
+    });
+
+module.exports = app;
